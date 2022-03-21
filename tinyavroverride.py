a0162c765bba0c732a684f9a689bd06928a5c567
ThreeRhinosInAnElephantCostume <myrealstemail@protonmail.com>
2022-03-21 12:28:56 +0100

    attiny13a support

--- tinyavroverride.py
+++ tinyavroverride.py
@@ -19,7 +19,7 @@ import os
 
 args = (" ".join(["\"" + it + "\"" if " " in it else it for it in sys.argv[1:]]))
 print(args)
-chips = [["t85", "attiny85"], ["t45", "attiny45"], ["t25", "attiny25"], ["t84", "attiny84"]]
+chips = [["t85", "attiny85"], ["t45", "attiny45"], ["t25", "attiny25"], ["t84", "attiny84"], ["t13", "attiny13"], ["t13a", "attiny13a"]]
 target = None
 for it in chips:
     if("-p " + it[0] in args or "-p " + it[1] in args):
